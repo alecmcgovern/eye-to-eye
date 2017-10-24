9e6ae375e78b2e3c251acfd3bf98567c5569c2d3
Alec M <alecmcgovern444@gmail.com>
2017-10-23 21:52:33 -0700

    deploy

--- src/components/App/api.js
+++ src/components/App/api.js
@@ -1,6 +1,7 @@
 import openSocket from 'socket.io-client';
 
-const socket = openSocket('http://localhost:8000');
+// const socket = openSocket('http://localhost:8000');
+const socket = openSocket(window.location.hostname);
 
 function subscribeToTimer(callback) {
 	socket.on('timer', timestamp => callback(null, timestamp));
