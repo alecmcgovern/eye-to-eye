8cfeefd274ac0f7a2b3d97bf79ed07a55c993f1d
Alec M <alecmcgovern444@gmail.com>
2017-10-23 21:41:47 -0700

    rearranging index.js file

--- src/components/App/api.js
+++ src/components/App/api.js
@@ -1,6 +1,6 @@
 import openSocket from 'socket.io-client';
 
-const socket = openSocket(window.location.hostname);
+const socket = openSocket('http://localhost:8000');
 
 function subscribeToTimer(callback) {
 	socket.on('timer', timestamp => callback(null, timestamp));
