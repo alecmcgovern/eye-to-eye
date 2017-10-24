f782d3971d34e703528115b6215d6a5a74dc3f9f
Alec M <alecmcgovern444@gmail.com>
2017-10-23 22:08:06 -0700

    updated gitignore

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ const io = require('socket.io')(server);
 
 const PORT = process.env.PORT || 8000;
 
-app.use(express.static(path.resolve(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('*', (req, res) => {
-	res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+	res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 
