16833113f8a017f2ac5e68868eff125cb6d08883
Alec M <alecmcgovern444@gmail.com>
2017-10-23 21:26:32 -0700

    change to host build

--- server/app.js
+++ server/app.js
@@ -7,10 +7,10 @@ const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
 
-app.use(express.static(path.resolve(__dirname, '..', 'public')));
+app.use(express.static(path.resolve(__dirname, '..', 'build')));
 
 app.get('*', (req, res) => {
-	res.sendFile(path.resolve(__dirname, '..', 'public', 'index.html'));
+	res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
 });
 
 
