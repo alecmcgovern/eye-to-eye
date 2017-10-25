f3bcb8c2c44d9b9d55fd20592661e6ef9d2bd7b3
Alec M <alecmcgovern444@gmail.com>
2017-10-25 14:44:20 -0700

    color controls

--- src/components/App/api.js
+++ src/components/App/api.js
@@ -1,7 +1,7 @@
 import openSocket from 'socket.io-client';
 
-const socket = openSocket('http://localhost:8000');
-// const socket = openSocket(window.location.hostname);
+// const socket = openSocket('http://localhost:8000');
+const socket = openSocket(window.location.hostname);
 
 function subscribeToMessages(callback) {
 	socket.on('messageReceived', messageReceived => callback(null, messageReceived));
--- src/components/App/canvas.js
+++ src/components/App/canvas.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { subscribeToMessages, sendSketch, subscribeToSketches, subscribeToShowImage, subscribeToClearCanvas } from './api';
-import ColorControls from './colorControls.js';
 
 import './canvas.css';
+import './colorControls.css';
 import datapoints from '../../images/datapoints.jpg';
 
 const canvasWidth = 1000;
@@ -24,12 +24,15 @@ class Canvas extends React.Component {
 		this.prevY = 0;
 		this.currY = 0;
 
-		this.style = "black";
+		this.color = "black";
 		this.flag = false;
 		this.dotFlag = false;
 		this.lineWidth = 2;
 
-		this.sketch = [];
+		this.sketch = {
+			color: this.color,
+			points: []
+		};
 
 		subscribeToMessages((err, message) => {
 			this.setState({
@@ -51,16 +54,16 @@ class Canvas extends React.Component {
 	}
 
 	drawSketch(sketch) {
-		for (let i = 0; i < sketch.length; i++) {
+		for (let i = 0; i < sketch.points.length; i++) {
 			if (i === 0) {
-				this.ctx.fillRect(sketch[0].x, sketch[0].y, 2, 2);
+				this.ctx.fillRect(sketch.points[0].x, sketch.points[0].y, 2, 2);
 			} else {
-				this.currX = sketch[i].x;
-				this.currY = sketch[i].y;
-				this.prevX = sketch[i-1].x;
-				this.prevY = sketch[i-1].y;
+				this.currX = sketch.points[i].x;
+				this.currY = sketch.points[i].y;
+				this.prevX = sketch.points[i-1].x;
+				this.prevY = sketch.points[i-1].y;
 
-				this.draw();
+				this.draw(sketch.color);
 			}
 		}
 	}
@@ -84,11 +87,11 @@ class Canvas extends React.Component {
 		this.canvas.addEventListener("touchleave", (e) => this.findxy(e, "out"));
 	}
 
-	draw() {
+	draw(color) {
 		this.ctx.beginPath();
 		this.ctx.moveTo(this.prevX, this.prevY);
 		this.ctx.lineTo(this.currX, this.currY);
-        this.ctx.strokeStyle = this.style;
+        this.ctx.strokeStyle = color;
         this.ctx.lineWidth = this.lineWidth;
         this.ctx.stroke();
         this.ctx.closePath();
@@ -104,19 +107,19 @@ class Canvas extends React.Component {
 
 		if (type === 'down') {
 			this.prevX = this.currX;
-			this.currX = e.clientX - this.canvas.offsetLeft + scrollLeft;
+			this.currX = e.clientX - this.canvas.parentElement.offsetLeft - this.canvas.offsetLeft + scrollLeft;
 
 			this.prevY = this.currY;
-			this.currY = e.clientY - this.canvas.offsetTop + scrollTop;
+			this.currY = e.clientY - this.canvas.parentElement.offsetTop - this.canvas.offsetTop + scrollTop;
 
 			this.flag = true;
 			this.dotFlag = true;
 
-			this.sketch.push({ x: this.currX, y: this.currY });
+			this.sketch.points.push({ x: this.currX, y: this.currY });
 
 			if (this.dotFlag) {
 				this.ctx.beginPath();
-				this.ctx.fillStyle = this.style;
+				this.ctx.fillStyle = this.color;
 				this.ctx.fillRect(this.currX, this.currY, this.lineWidth, this.lineWidth);
 				this.ctx.closePath();
 				this.dotFlag = false;
@@ -125,19 +128,19 @@ class Canvas extends React.Component {
 
 		if (type === 'touchstart' && e.touches.length < 2) {
 			this.prevX = this.currX;
-			this.currX = e.touches[0].clientX - this.canvas.offsetLeft + scrollLeft;
+			this.currX = e.touches[0].clientX - this.canvas.parentElement.offsetLeft - this.canvas.offsetLeft + scrollLeft;
 
 			this.prevY = this.currY;
-			this.currY = e.touches[0].clientY - this.canvas.offsetTop + scrollTop;
+			this.currY = e.touches[0].clientY - this.canvas.parentElement.offsetTop - this.canvas.offsetTop + scrollTop;
 
 			this.flag = true;
 			this.dotFlag = true;
 
-			this.sketch.push({ x: this.currX, y: this.currY });
+			this.sketch.points.push({ x: this.currX, y: this.currY });
 
 			if (this.dotFlag) {
 				this.ctx.beginPath();
-				this.ctx.fillStyle = this.style;
+				this.ctx.fillStyle = this.color;
 				this.ctx.fillRect(this.currX, this.currY, this.lineWidth, this.lineWidth);
 				this.ctx.closePath();
 				this.dotFlag = false;
@@ -146,36 +149,45 @@ class Canvas extends React.Component {
 
 		if (type === 'up' || type === 'out') {
 			this.flag = false;
-
-			sendSketch(this.sketch);
-			this.sketch = [];
+			if (this.sketch && this.sketch.points.length > 0) {
+				sendSketch(this.sketch);
+			}
+			this.sketch = {
+				color: this.color,
+				points: []
+			};
 		}
 
 		if (type === 'move') {
 			if (this.flag) {
 				this.prevX = this.currX;
-				this.currX = e.clientX - this.canvas.offsetLeft + scrollLeft;
+				this.currX = e.clientX - this.canvas.parentElement.offsetLeft - this.canvas.offsetLeft + scrollLeft;
 
 				this.prevY = this.currY;
-				this.currY = e.clientY - this.canvas.offsetTop + scrollTop;
+				this.currY = e.clientY - this.canvas.parentElement.offsetTop - this.canvas.offsetTop + scrollTop;
 
-				this.sketch.push({ x: this.currX, y: this.currY });
-				this.draw();
+				this.sketch.points.push({ x: this.currX, y: this.currY });
+				this.draw(this.color);
 			}
 		}
 
 		if (type === 'touchmove' && e.touches.length < 2) {
 			if (this.flag) {
 				this.prevX = this.currX;
-				this.currX = e.touches[0].clientX - this.canvas.offsetLeft + scrollLeft;
+				this.currX = e.touches[0].clientX - this.canvas.parentElement.offsetLeft - this.canvas.offsetLeft + scrollLeft;
 
 				this.prevY = this.currY;
-				this.currY = e.touches[0].clientY - this.canvas.offsetTop + scrollTop;
+				this.currY = e.touches[0].clientY - this.canvas.parentElement.offsetTop - this.canvas.offsetTop + scrollTop;
 
-				this.sketch.push({ x: this.currX, y: this.currY });
-				this.draw();
+				this.sketch.points.push({ x: this.currX, y: this.currY });
+				this.draw(this.color);
 			}
 		}
+	}
+
+	selectColor(color) {
+		this.color = color;
+		this.sketch.color = color;
 	}
 
 
@@ -186,6 +198,15 @@ class Canvas extends React.Component {
 			canvasBackgroundImageClass += " image-hide";
 		}
 
+		let color1Class = "color-control color-1";
+		let color2Class = "color-control color-2";
+		let color3Class = "color-control color-3";
+		let color4Class = "color-control color-4";
+		let color5Class = "color-control color-5";
+		let color6Class = "color-control color-6";
+		let color7Class = "color-control color-7";
+		let color8Class = "color-control color-8";
+
 		return (
 			<div className="canvas-container">
 				<img className={canvasBackgroundImageClass} src={datapoints} alt=""/>
@@ -193,7 +214,16 @@ class Canvas extends React.Component {
 					{this.state.string}
 				</div>
 				<canvas width={canvasWidth + "px"} height={canvasHeight + "px"} ref="canvas" id="canvas"></canvas>
-				<ColorControls />
+				<div className="color-controls-container">
+					<div className={color1Class} onClick={() => this.selectColor("black")}></div>
+					<div className={color2Class} onClick={() => this.selectColor("white")}></div>
+					<div className={color3Class} onClick={() => this.selectColor("red")}></div>
+					<div className={color4Class} onClick={() => this.selectColor("orange")}></div>
+					<div className={color5Class} onClick={() => this.selectColor("yellow")}></div>
+					<div className={color6Class} onClick={() => this.selectColor("green")}></div>
+					<div className={color7Class} onClick={() => this.selectColor("blue")}></div>
+					<div className={color8Class} onClick={() => this.selectColor("purple")}></div>
+				</div>
 			</div>
 		);
 	}
